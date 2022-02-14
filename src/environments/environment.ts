f0d5dc085ae3aba21fb964f6327b90335a01a9af
KK <karanjakelvin77@gmail.com>
2022-02-14 06:33:48 +0300

    It will remove the API key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  apiKey: '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'
+  production: false
 };
 
 /*
