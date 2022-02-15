daf3c36c8aaefbe0ab0c71bfe90d9ec5c1f9bdd0
KK <karanjakelvin77@gmail.com>
2022-02-15 12:33:30 +0300

    It will add and define the fucntions to search username and repositories

--- src/app/services/githubapi.service.ts
+++ src/app/services/githubapi.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Users } from '../Classes/users';
 import { Repositories } from '../Classes/repositories';
 import { BrowserModule } from '@angular/platform-browser';
@@ -11,14 +11,30 @@ import { map } from 'rxjs/operators';
 })
 export class GithubapiService {
 
-  username?: string;
+  username: string;
+  clientId = '367fc5193fa0c21f98ab'
+  clientSecret = '567fe7fb66026ad1124c93b18bcf0e34fb74e936'
 
 
 
   constructor(private http: HttpClient) {
+    this.username = 'KaranjaK'
   }
 
-  searchUser(term: string) {
+  searchUser() {
+    interface apiFeedback {
+      login: string
+    }
+    return this.http.get('https://api.github.com/users/' + this.username + '?client_id=' + this.clientId + '&client_secret=' + this.clientSecret)
+      // .pipe(map(res => res.json()))
+  }
+  searchRepo() {
+    return this.http.get('https://api.github.com/repositories/' + this.username + '/repos?client_id=' + this.clientId + '&client_secret=' + this.clientSecret)
+      // .pipe(map(res => res.json()))
+  }
+
+  profileUpdate (username: string){
+    this.username = username;
   }
 
 }
