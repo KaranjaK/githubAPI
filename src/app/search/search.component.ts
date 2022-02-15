1336161f2d4c803b2ee46faaa7e37233722ea64d
KK <karanjakelvin77@gmail.com>
2022-02-15 14:04:28 +0300

    It will define the functions to be used in the search form

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { profile } from 'console';
 import { GithubapiService } from '../services/githubapi.service';
 
 @Component({
@@ -8,15 +9,41 @@ import { GithubapiService } from '../services/githubapi.service';
 })
 export class SearchComponent implements OnInit {
 
-  users!: String[];
+  userData: any = {
+    login: '',
+    company: '',
+    location: '',
+    avatar_url: '',
+    public_repos: '',
+    public_gist: '',
+    followers: '',
+    following: '',
+    email: '',
+    bio: '',
+    created_at: ''
+  };
+  repos!: any;
+  username!: string;
 
-  constructor(private githubservice: GithubapiService) { }
+  constructor(private githubservice: GithubapiService) { 
+    this.githubservice.searchUser().subscribe(userData => {
+      this.userData = userData 
+    })
+    this.githubservice.searchRepo().subscribe(repos => {
+      this.repos = repos
+    })
+  }
+   doUserSearch() {
+    this.githubservice.profileUpdate(this.username)
+    this.githubservice.searchUser().subscribe(userData => {
+      this.userData = this.userData
+    })
+    this.githubservice.searchRepo().subscribe(repos => {
+      this.repos = repos
+    })
+  }
 
   ngOnInit(): void {
   }
 
-  doUserSearch(term: string){
-    this.githubservice.searchUser(term)
-  }
-  
 }
