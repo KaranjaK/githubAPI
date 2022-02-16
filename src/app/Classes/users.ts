a6b622a091873db01c236ea1a8e3213d1ecd619f
KK <karanjakelvin77@gmail.com>
2022-02-16 08:42:55 +0300

    It will add an export class for the pipe

--- src/app/Classes/users.ts
+++ src/app/Classes/users.ts
@@ -3,3 +3,7 @@ export class Users {
     profPic?: String;
     repoList!: String[];
 }
+export class Constants {
+  static readonly DATE_FMT = 'dd/MMM/yyyy';
+  static readonly DATE_TIME_FMT = `${Constants.DATE_FMT} hh:mm:ss`;
+}
