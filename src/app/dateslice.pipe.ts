ce06c58f6c13a8743b2a5f12ab59634c4a0f4692
KK <karanjakelvin77@gmail.com>
2022-02-16 08:43:32 +0300

    It will detail what the pipe is to do

--- src/app/dateslice.pipe.ts
+++ src/app/dateslice.pipe.ts
@@ -1,12 +1,14 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { Constants } from './Classes/users';
+import { DatePipe } from '@angular/common';
 
 @Pipe({
   name: 'dateslice'
 })
-export class DateslicePipe implements PipeTransform {
+export class DateslicePipe extends DatePipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  override transform(value: any, args?: any): any {
+    return super.transform(value, Constants.DATE_TIME_FMT);;
   }
 
 }
